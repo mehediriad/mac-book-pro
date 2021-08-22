ed5f4aa266f5b4b842fc07a8858fca4baa047015
mehedi_riad <mhriad786@gmail.com>
2021-08-22 09:20:22 +0600

    final commit

--- script.js
+++ script.js
@@ -1,21 +1,78 @@
+let memoryPrice = document.getElementById('extra-memory');
+let storagePrice = document.getElementById('extra-storage');
+let delivery = document.getElementById('delivery-charge');
+let totalCost = document.getElementById('total-price');
 
-function getExtraCost(eventId ,costId , costValue){
-    document.getElementById(eventId).addEventListener('click',function(){
-        let costField = document.getElementById(costId);
-        costField.innerText = costValue;
-        const bestPrice = document.getElementById('best-price').innerText;
+function GetMemoryValue(x,id){
+    memoryPrice.innerText = x;
+    showTotalCost ()
+ }
+
+ function GetStorageValue(x,id){
+    storagePrice.innerText = x;
+    showTotalCost ()
+ }
+function GetDeliveryValue(s){
+    delivery.innerText = s;
+    showTotalCost ()
+ }
+ function showTotalCost (){
+     console.log(memoryPrice)
+     let total =1299 + Number(memoryPrice.innerText) + Number(storagePrice.innerText) +Number(delivery.innerText);
+     document.getElementById('total-price').innerText = total;
+     document.getElementById('total').innerText = `Total: $ ${total}`;
+
+  }
+  function proCode(){
+    const promoCode = document.getElementById('promuValue').value ;
+    if(promoCode == 'stevekaku'){
+        let price =    totalCost.innerText;
+        let percentage = Number(price) * 20 /100
+         document.getElementById('total').innerText = Number(price)- percentage;
+
+     }
+   }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function getExtraCost(eventId ,costId , costValue){
+//     document.getElementById(eventId).addEventListener('click',function(){
+//         let costField = document.getElementById(costId);
+//         costField.innerText = costValue;
+//         const bestPrice = document.getElementById('best-price').innerText;
         
-        document.getElementById('total-price').innerText = parseInt(bestPrice) + costValue
+//         document.getElementById('total-price').innerText = parseInt(bestPrice) + costValue
 
-    })
-}
-getExtraCost('memory-8gb', 'extra-memory', 0)
-getExtraCost('memory-16gb', 'extra-memory', 180)
-getExtraCost('storage-256gb', 'extra-storage', 0)
-getExtraCost('storage-512gb', 'extra-storage', 100)
-getExtraCost('storage-1tb', 'extra-storage', 180)
-getExtraCost('delivery-free', 'delivery-charge', 0)
-getExtraCost('delivery-cost', 'delivery-charge', 20)
+//     })
+// }
+// getExtraCost('memory-8gb', 'extra-memory', 0)
+// getExtraCost('memory-16gb', 'extra-memory', 180)
+// getExtraCost('storage-256gb', 'extra-storage', 0)
+// getExtraCost('storage-512gb', 'extra-storage', 100)
+// getExtraCost('storage-1tb', 'extra-storage', 180)
+// getExtraCost('delivery-free', 'delivery-charge', 0)
+// getExtraCost('delivery-cost', 'delivery-charge', 20)
 
 
 
@@ -30,6 +87,8 @@ getExtraCost('delivery-cost', 'delivery-charge', 20)
 //         document.getElementById('extra-memory').innerText = 180;
 //     }
 // }
+
+
 
 // document.getElementById('memory-8gb').addEventListener('click',function(){
 //     document.getElementById('extra-memory').innerText = 0
